cbf78f850b5595b0654f6e307ebdac60d590a8c6
Whitley Beers <whitleybeers@gmail.com>
2023-07-31 21:21:04 -0500

    merging Dede's branch

--- insurance/settings.py
+++ insurance/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&d$49wj$h!3l0p+kg%#$e+ijqzru*#m7n)!mak9u^8rex#kv#r'
+SECRET_KEY = 'django-insecure-2&6v1(5)9=m491c$xwh%(wjcbpz+5mkelsr@q+pz0w_6yn2i1l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
